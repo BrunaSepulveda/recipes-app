fc986bd4adbfa1869f94beedcdb7be81fc7f92fe
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-06-07 16:57:13 -0300

    Documents checked and corrected

--- pages/api/categories/[name].js
+++ pages/api/categories/[name].js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export default async function getMealsByNameCategory(req, res) {
+export default async function getMealsByCategoryName(req, res) {
   const { name } = req.query
-  const response  = await axios.get(`https://themealdb.com/api/json/v1/1/search.php?s=${name}`);
+  const response = await axios.get(`https://themealdb.com/api/json/v1/1/search.php?s=${name}`);
   const recipes = response.data.meals.map((recipe) => {
     return {
       id: recipe.idMeal,
@@ -13,27 +13,3 @@ export default async function getMealsByNameCategory(req, res) {
   });
   res.status(200).json(recipes);
 }
-
-
-/* 
-const numbers = [12, 5, 8, 130, 44];
-filter, some, find
-
- numbers.every(function(num, index, array) {
-  return num >= 10;;
-});
-
-numbers.reduce(function(acumulador, valorAtual, index, array) {
-  return acumulador + valorAtual;
-})
-
-
-  propNames.forEach(function(element, index, array){
-     console.log("a[" + index + "] = " + element);
-  });
-
-
- numbers.map(function(num) {
-  return num * 2;
-});
-*/
--- pages/api/categories/index.js
+++ pages/api/categories/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function getAllCategories(_req, res) {
-  const response  = await axios.get('https://themealdb.com/api/json/v1/1/categories.php');
+  const response = await axios.get('https://themealdb.com/api/json/v1/1/categories.php');
   const categories = response.data.categories.map((category) => {
     return {
       id: category.idCategory,
